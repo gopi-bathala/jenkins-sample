be5b250611849aa2360a0b7794672664727b729c
gopi-bathala <31345007+gopi-bathala@users.noreply.github.com>
2021-06-11 08:43:30 +0400

    Add files via upload

---
+++ src/test/java/com/gopi/JenkinsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
